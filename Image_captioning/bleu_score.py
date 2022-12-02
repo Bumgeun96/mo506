2104a6c2fddf4b5a3f5a879c4baea2e179c2d8f8
j4t123@naver.com <j4t123@naver.com>
2022-12-02 11:31:02 +0900

    score function

--- Image_captioning/bleu_score.py
+++ Image_captioning/bleu_score.py
@@ -2,7 +2,7 @@ from nltk.translate.bleu_score import sentence_bleu
 
 x = 'I am a good boy!'
 y = 'I am a boy!'
-
-score = sentence_bleu([x], y, weights=(1, 0, 0, 0)) 
-
+def score(x,y):
+    score = sentence_bleu([x], y, weights=(1, 0, 0, 0)) 
+    return score
 print(score)
